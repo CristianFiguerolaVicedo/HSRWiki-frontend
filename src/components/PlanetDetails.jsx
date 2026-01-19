90f5219fdac5065348e423a166587570ad458ec9
CriFigShe <crifigshe@gmail.com>
2026-01-19 16:34:51 +0100

    Carrousel images for planets

--- src/components/PlanetDetails.jsx
+++ src/components/PlanetDetails.jsx
@@ -1,11 +1,90 @@
 import { useParams } from "react-router-dom";
 import { PLANETS_DATA } from "../util/util";
 import { useState } from "react";
+import { ArrowLeft, ArrowRight, ChevronLeft, ChevronRight } from "lucide-react";
 
 const PlanetDetails = () => {
-    const [loading, setLoading] = useState(false);
+    const [loading] = useState(false);
     const {id} = useParams();
-    const planet = PLANETS_DATA.find(p => p.id === id);
+    const planet = PLANETS_DATA.find((p) => p.id === id);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    const IMAGES_CARROUSEL = {
+        "Herta Space Station": [
+            "/public/assets/herta1.png",
+            "/public/assets/herta2.png",
+            "/public/assets/herta3.png",
+            "/public/assets/herta4.png",
+            "/public/assets/herta5.png",
+            "/public/assets/herta6.png",
+        ],
+        "Jarilo VI": [
+            "/public/assets/jarilo1.png",
+            "/public/assets/jarilo2.png",
+            "/public/assets/jarilo3.png",
+            "/public/assets/jarilo4.png",
+            "/public/assets/jarilo5.png",
+            "/public/assets/jarilo6.png",
+        ],
+        "The Xianzhou Luofu": [
+            "/public/assets/xianzhou1.png",
+            "/public/assets/xianzhou2.png",
+            "/public/assets/xianzhou3.png",
+            "/public/assets/xianzhou4.png",
+            "/public/assets/xianzhou5.png",
+            "/public/assets/xianzhou6.png",
+            "/public/assets/xianzhou7.png",
+            "/public/assets/xianzhou8.png",
+            "/public/assets/xianzhou9.png",
+        ],
+        "Penacony": [
+            "/public/assets/penacony1.png",
+            "/public/assets/penacony2.png",
+            "/public/assets/penacony3.png",
+            "/public/assets/penacony4.png",
+            "/public/assets/penacony5.png",
+            "/public/assets/penacony6.png",
+            "/public/assets/penacony7.png",
+            "/public/assets/penacony8.png",
+            "/public/assets/penacony9.png",
+            "/public/assets/penacony10.png",
+            "/public/assets/penacony11.png",
+        ],
+        "Amphoreus": [
+            "/public/assets/amph1.png",
+            "/public/assets/amph2.png",
+            "/public/assets/amph3.png",
+            "/public/assets/amph4.png",
+            "/public/assets/amph5.png",
+            "/public/assets/amph6.png",
+            "/public/assets/amph7.png",
+            "/public/assets/amph8.png",
+            "/public/assets/amph9.png",
+            "/public/assets/amph10.png",
+            "/public/assets/amph11.png",
+            "/public/assets/amph12.png",
+            "/public/assets/amph13.png",
+            "/public/assets/amph14.png",
+            "/public/assets/amph15.png",
+            "/public/assets/amph16.png",
+            "/public/assets/amph17.png",
+        ]
+    };
+
+    const planetImages = IMAGES_CARROUSEL[planet.name] || [];
+
+    const handlePrev = () => {
+        setCurrentImageIndex((prev) =>
+            prev === 0 ? planetImages.length - 1 : prev - 1
+        );
+    };
+
+    const handleNext = () => {
+        setCurrentImageIndex((prev) =>
+            prev === planetImages.length - 1 ? 0 : prev + 1
+        );
+    };
+
 
     if (loading) {
         return (
@@ -22,6 +101,7 @@ const PlanetDetails = () => {
             </div>
         )
     }
+
     return (
         <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-950 text-white p-6">
             <div className="max-w-4xl mx-auto">
@@ -29,11 +109,31 @@ const PlanetDetails = () => {
                     {planet.name}
                 </h1>
 
-                <img
-                    src={`/public/assets/${planet.name}.jpg`}
-                    alt={planet.name}
-                    className="w-full max-h-96 object-cover rounded-lg mb-6"
-                />
+                {planetImages.length > 0 ? (
+                    <div className="relative mb-6">
+                        <img 
+                            src={planetImages[currentImageIndex]} 
+                            alt={`${planet.name} ${currentImageIndex + 1}`} 
+                            className="w-full max-h-96 object-cover rounded-lg"
+                        />
+
+                        <button
+                            onClick={handlePrev}
+                            className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-gray-800 bg-opacity-50 text-white p-2 rounded-full hover:cursor-pointer transition-all duration-[300ms] hover:scale-[1.1]"
+                        >
+                            <ChevronLeft />
+                        </button>
+
+                        <button
+                            onClick={handleNext}
+                            className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-gray-800 bg-opacity-50 text-white p-2 rounded-full hover:cursor-pointer transition-all duration-[300ms] hover:scale-[1.1]"
+                        >
+                            <ChevronRight />
+                        </button>
+                    </div>
+                ) : (
+                    <div>No va</div>
+                )}
 
                 <p className="text-gray-300 leading-relaxed mb-3">
                     {planet.description1}
